1928dd55c802d3075d0dfe8ce1ef850ed03940cf
amym321 <66137093+amym321@users.noreply.github.com>
2020-10-13 15:03:40 -0500

    Updates

---
+++ precache-manifest.34e00bfb82a1bba556cca0ecf088135d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d93adbaf2825719880db032c92d614d",
+    "url": "/Lebowski_Cast2/index.html"
+  },
+  {
+    "revision": "b5daf879d49b08fd23f8",
+    "url": "/Lebowski_Cast2/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f3d4215905261723f4d0",
+    "url": "/Lebowski_Cast2/static/css/main.de73bc46.chunk.css"
+  },
+  {
+    "revision": "b5daf879d49b08fd23f8",
+    "url": "/Lebowski_Cast2/static/js/2.6c5aae56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lebowski_Cast2/static/js/2.6c5aae56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d4215905261723f4d0",
+    "url": "/Lebowski_Cast2/static/js/main.901bf0de.chunk.js"
+  },
+  {
+    "revision": "ce33994bd160403569a8",
+    "url": "/Lebowski_Cast2/static/js/runtime-main.8e0cb6e0.js"
+  },
+  {
+    "revision": "6f6d9b4ddc58ef753647dd335d65395b",
+    "url": "/Lebowski_Cast2/static/media/Diavlo_BLACK_II_37.6f6d9b4d.woff"
+  }
+]);
